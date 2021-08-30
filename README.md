b81e0e68e1d0a5f4053884001bb5c2f74ff62df0
Leonardo Olmedo <lgo80@yahoo.com.ar>
2021-08-29 23:21:09 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Parcial de Pdep
+
+Las Casas de Hogwarts - Hecho por Juan Contardo y Fernando Dodino.
+
+Paradigma logico - Lenguaje Prolog
---
+++ solucion.pl
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Parcial - Las Casas de Hogwarts
+% NOMBRE: Leonardo Olmedo - lgo1980
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+
+:- begin_tests(utneanos).
+
+  test(a_que_persona_le_gusta_cavenagui,set(Persona=[gabriel])):-
+    creeEn(Persona,cavenaghi).
+
+:- end_tests(utneanos).
